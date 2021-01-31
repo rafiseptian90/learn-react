8bab8c9825bb8a006e7fc2fbf61c1714d3a2945b
Rafi Septian Hadi <rafiseptian90@gmail.com>
2021-01-31 12:01:11 +0700

    adding detail post

--- src/views/BLogDetail.js
+++ src/views/BLogDetail.js
@@ -1,11 +1,30 @@
 import { useParams } from 'react-router-dom'
+import useFetch from "../hooks/useFetch";
 
 const BLogDetail = () => {
     const { id } = useParams()
+    const { data: post, isLoading, error } = useFetch(`http://localhost:8000/blogs/${id}`)
 
     return(
         <div className="container px-12 py-14">
-            <h1 className="text-2xl font-semibold text-gray-600">Blog Detail - { id }</h1>
+            {
+                /* show simple loading */
+                isLoading && <div className="text-lg text-center">Loading...</div>
+            }
+
+            {
+                post
+                &&
+                <div className="content">
+                    <h1 className="text-2xl font-semibold text-gray-600 mb-3">{post.title}</h1>
+                    <p className="text-base text-gray-700">
+                        { post.body }
+                    </p>
+                    <span className="block italic font-light mt-5">
+                        { post.author }
+                    </span>
+                </div>
+            }
         </div>
     )
 }
